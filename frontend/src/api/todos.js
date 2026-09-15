2dc55271fb0aec48fdac609a90c48ba1553ef16b
markemad16008611 <markemaad20062@gmail.com>
2026-09-15 04:15:18 +0300

    Refactor fetchTodos to support filtering

--- frontend/src/api/todos.js
+++ frontend/src/api/todos.js
@@ -5,7 +5,21 @@ const api = axios.create({
   baseURL: 'http://localhost:3000/api/todos'
 });
 
-export const fetchTodos = () => api.get('/').then(res => res.data);
+export const fetchTodos = async (filter) => {
+    const params = {};
+
+    if (filter === "done") {
+        params.done = true;
+    }
+
+    if (filter === "active") {
+        params.done = false;
+    }
+
+    const response = await api.get("/", { params });
+
+    return response.data;
+};
 
 export const createTodo = (title) =>
   api.post('/', { title }).then(res => res.data);
