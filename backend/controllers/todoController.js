5636401ca1db6882879551e70c34ce56c15e7d3d
markemad16008611 <markemaad20062@gmail.com>
2026-09-15 04:27:37 +0300

    Add filtering for todos based on 'done' status

--- backend/controllers/todoController.js
+++ backend/controllers/todoController.js
@@ -1,14 +1,19 @@
 const Todo = require('../models/Todo');
 
 const getTodos = async (req, res) => {
-  try {
-    const todos = await Todo.find().sort({ createdAt: -1 });
-    res.json(todos);
-  } catch (err) {
-    res.status(500).json({ error: 'Server error' });
-  }
-};
+    try {
+        const filter = {};
 
+        if (req.query.done !== undefined) {
+            filter.done = req.query.done === "true";
+        }
+
+        const todos = await Todo.find(filter).sort({ createdAt: -1 });
+        res.json(todos);
+    } catch (err) {
+        res.status(500).json({ error: 'Server error' });
+    }
+};
 const createTodo = async (req, res) => {
   if (!req.body.title) return res.status(400).json({ error: 'Title is required' });
   
