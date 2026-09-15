33560f8d6212c0dec0acfa328fe197b14e5500a9
markemad16008611 <markemaad20062@gmail.com>
2026-09-15 04:03:18 +0300

    Enhance getTodos with filtering options
    Refactor getTodos to include filtering by 'done' status.

--- backend/controllers/todoController.js
+++ backend/controllers/todoController.js
@@ -1,23 +1,19 @@
 const Todo = require('../models/Todo');
 
-const getTodos = async (req, res) => {
-  try {
-    const todos = await Todo.find().sort({ createdAt: -1 });
-    res.json(todos);
-  } catch (err) {
-    res.status(500).json({ error: 'Server error' });
-  }
-};
 
-const createTodo = async (req, res) => {
-  if (!req.body.title) return res.status(400).json({ error: 'Title is required' });
-  
-  try {
-    const newTodo = await Todo.create({ title: req.body.title });
-    res.status(201).json(newTodo);
-  } catch (err) {
-    res.status(500).json({ error: 'Server error' });
-  }
+const getTodos = async (req, res) => {
+    try {
+        const filter = {};
+
+        if (req.query.done !== undefined) {
+            filter.done = req.query.done === "true";
+        }
+
+        const todos = await Todo.find(filter).sort({ createdAt: -1 });
+        res.json(todos);
+    } catch (err) {
+        res.status(500).json({ error: 'Server error' });
+    }
 };
 
 const updateTodo = async (req, res) => {
